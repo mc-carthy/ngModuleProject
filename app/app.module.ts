64910ca17a20c64cea9407335e6d5c748fc5c770
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-05-30 19:35:19 +0100

    Further refactoring to make use of models (using app.module for providers & imports)

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { MessagesComponent } from './messages.component';
 
+import { MessagesService } from './messages.service';
+
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, FormsModule ],
   declarations: [
     AppComponent,
     MessagesComponent
   ],
+  providers: [ MessagesService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- app/messages.component.ts
+++ app/messages.component.ts
@@ -1,15 +1,24 @@
 
 import { Component } from '@angular/core';
 
+import { MessagesService } from './messages.service'
+
 @Component({
     selector: 'messages',
     template: `
         <h1>Messages</h1>
+        <input type="text" [(ngModel)]="title" />
         <ul>
             <li *ngFor="let m of messages">{{ m }}</li>
         </ul>
     `
 })
 export class MessagesComponent {
-    messages = [ "m1", "m2", "m3 "];
+    messages;
+    title = "Hello World!";
+
+    constructor(private _service: MessagesService)
+    {
+        this.messages = _service.getMessages();
+    }
 }
